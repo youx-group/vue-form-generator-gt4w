304cc59dc1156b5dd15dfd97f0dc6fc98cc40ff0
Leonardo Almeida <leonardoalmeida.gt4w@gmail.com>
2020-04-13 09:47:39 -0300

    Adapta input para o Element

--- src/fields/core/fieldInput.vue
+++ src/fields/core/fieldInput.vue
@@ -1,49 +1,54 @@
 <template lang="pug">
-.wrapper(v-attributes="'wrapper'")
-	input.form-control(
-		:id="getFieldID(schema)",
-		:type="inputType",
-		:value="value",
-		@input="onInput",
-		@blur="onBlur",
-		:class="schema.fieldClasses",
-		@change="schema.onChange || null",
-		:disabled="disabled",
-		:accept="schema.accept",
-		:alt="schema.alt",
-		:autocomplete="schema.autocomplete",
-		:checked="schema.checked",
-		:dirname="schema.dirname",
-		:formaction="schema.formaction",
-		:formenctype="schema.formenctype",
-		:formmethod="schema.formmethod",
-		:formnovalidate="schema.formnovalidate",
-		:formtarget="schema.formtarget",
-		:height="schema.height",
-		:list="schema.list",
-		:max="schema.max",
-		:maxlength="schema.maxlength",
-		:min="schema.min",
-		:minlength="schema.minlength",
-		:multiple="schema.multiple",
-		:name="schema.inputName",
-		:pattern="schema.pattern",
-		:placeholder="schema.placeholder",
-		:readonly="schema.readonly",
-		:required="schema.required",
-		:size="schema.size",
-		:src="schema.src",
-		:step="schema.step",
-		:width="schema.width",
-		:files="schema.files"
-		v-attributes="'input'")
-	span.helper(v-if="schema.inputType.toLowerCase() === 'color' || schema.inputType.toLowerCase() === 'range'") {{ value }}
+el-col(:xs="schema.xs", :sm="schema.sm", :lg="schema.lg", :xl="schema.xl")
+	.wrapper(v-attributes="'wrapper'")
+		el-input(
+			:id="getFieldID(schema)",
+			:type="inputType",
+			:value="value",
+			@input="onInput",
+			@blur="onBlur",
+			:class="schema.fieldClasses",
+			@change="schema.onChange || null",
+			:disabled="disabled",
+			:accept="schema.accept",
+			:alt="schema.alt",
+			:autocomplete="schema.autocomplete",
+			:checked="schema.checked",
+			:dirname="schema.dirname",
+			:formaction="schema.formaction",
+			:formenctype="schema.formenctype",
+			:formmethod="schema.formmethod",
+			:formnovalidate="schema.formnovalidate",
+			:formtarget="schema.formtarget",
+			:height="schema.height",
+			:list="schema.list",
+			:max="schema.max",
+			:maxlength="schema.maxlength",
+			:min="schema.min",
+			:minlength="schema.minlength",
+			:multiple="schema.multiple",
+			:name="schema.inputName",
+			:pattern="schema.pattern",
+			:placeholder="schema.placeholder",
+			:readonly="schema.readonly",
+			:required="schema.required",
+			:size="schema.size",
+			:src="schema.src",
+			:step="schema.step",
+			:width="schema.width",
+			:files="schema.files"
+			v-attributes="'input'")
+		span.helper(v-if="schema.inputType.toLowerCase() === 'color' || schema.inputType.toLowerCase() === 'range'") {{ value }}
 </template>
 
 <script>
 import abstractField from "../abstractField";
 import { debounce, get as objGet, isFunction, isNumber } from "lodash";
 import fecha from "fecha";
+
+import Vue from 'vue';
+import { Input } from 'element-ui';
+Vue.use(Input);
 
 const DATETIME_FORMATS = {
 	date: "YYYY-MM-DD",
