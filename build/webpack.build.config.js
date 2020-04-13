1ed234302b25d1436463e424a82b4fa4b4c2af8d
Leonardo Almeida <leonardoalmeida.gt4w@gmail.com>
2020-04-13 09:46:13 -0300

    Configura webpack para aceitar o Element

--- build/webpack.build.config.js
+++ build/webpack.build.config.js
@@ -47,6 +47,14 @@ let rules = [
 		test: /\.(ttf|eot)$/,
 		loader: "url-loader",
 		include: [path.resolve("src")]
+	},
+	{
+		test: /\.css$/,
+		loader: 'style-loader!css-loader'
+	},
+	{
+		test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+		loader: 'file-loader'
 	}
 ];
 
--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -35,6 +35,14 @@ let rules = [
 		test: /\.(ttf|eot)$/,
 		loader: "url-loader",
 		include: [path.resolve("src"), path.resolve("dev")]
+	},
+	{
+		test: /\.css$/,
+		loader: 'style-loader!css-loader'
+	},
+	{
+		test: /\.(eot|svg|ttf|woff|woff2)(\?\S*)?$/,
+		loader: 'file-loader'
 	}
 ];
 
